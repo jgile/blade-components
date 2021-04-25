6f06d8a56270c4e9332a120542cf48424c392eef
John Gile <me@jgile.com>
2021-04-25 11:46:50 -0700

    wip

--- src/Views/Components/InputGroup.php
+++ src/Views/Components/InputGroup.php
@@ -9,12 +9,14 @@ class InputGroup extends Component
     public $for;
     public $label;
     public $validate;
+    public $description;
 
-    public function __construct($for = null, $label = null, $validate = null)
+    public function __construct($for = null, $label = null, $validate = null, $description = null)
     {
         $this->for = $for;
         $this->validate = $validate === null ? $for : $validate;
         $this->label = $label;
+        $this->description = $description;
     }
 
     public function render()
