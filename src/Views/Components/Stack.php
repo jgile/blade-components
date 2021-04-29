527bc1f4b156ca8aa039c1640f262bf841d0de06
John Gile <me@jgile.com>
2021-04-28 20:30:35 -0700

    wip
    wip

--- src/Views/Components/Stack.php
+++ src/Views/Components/Stack.php
@@ -3,46 +3,68 @@
 namespace JGile\BladeComponents\Views\Components;
 
 use Illuminate\View\Component;
-use JGile\BladeComponents\ClassCollection;
 
 class Stack extends Component
 {
-    public ClassCollection $classCollection;
+    public string $classes;
 
     public function __construct(
         bool $vertical = false,
         bool $reverse = false,
         bool $center = false,
         bool $stretch = false,
-        bool $start = false,
-        bool $end = false,
         bool $between = false,
         bool $even = false,
         bool $around = false,
         bool $grow = false,
         bool $shrink = false,
+        bool $right = false,
+        bool $left = false,
+        bool $top = false,
+        bool $bottom = false,
+        bool $end = false,
+        bool $start = false,
+        bool $wrap = false,
         $space = null
     )
     {
-        $this->classCollection = new ClassCollection(['flex']);
+        $this->classes = 'flex';
 
         if ($vertical) {
-            $this->classCollection->pushIf($reverse, 'flex-col-reverse', 'flex-col');
-            $this->classCollection->pushIf($space, $space === true ? "space-y-2" : "space-y-$space");
+            $this->appendIf($reverse, 'flex-col-reverse', 'flex-col');
+            $this->appendIf($space, $space === true ? "space-y-2" : "space-y-$space");
+            $this->appendIf($right, 'items-end');
+            $this->appendIf($left, 'items-start');
+            $this->appendIf($bottom, 'justify-end');
+            $this->appendIf($top, 'justify-start');
         } else {
-            $this->classCollection->pushIf($reverse, 'flex-row-reverse', 'flex-row');
-            $this->classCollection->pushIf($space, $space === true ? "space-x-2" : "space-x-$space");
+            $this->appendIf($reverse, 'flex-row-reverse');
+            $this->appendIf($space, $space === true ? "space-x-2" : "space-x-$space");
+            $this->appendIf($right, 'justify-end');
+            $this->appendIf($left, 'justify-start');
+            $this->appendIf($top, 'content-start');
+            $this->appendIf($bottom, 'content-end');
         }
 
-        $this->classCollection->pushIf($end, 'items-end');
-        $this->classCollection->pushIf($start, 'items-start');
-        $this->classCollection->pushIf($stretch, 'items-stretch');
-        $this->classCollection->pushIf($center, 'items-center');
-        $this->classCollection->pushIf($even, 'justify-evenly');
-        $this->classCollection->pushIf($between, 'justify-between');
-        $this->classCollection->pushIf($around, 'justify-around');
-        $this->classCollection->pushIf($grow, 'flex-grow');
-        $this->classCollection->pushIf($shrink, 'flex-shrink');
+        $this->appendIf($wrap, 'flex-wrap');
+        $this->appendIf($end, 'items-end');
+        $this->appendIf($start, 'items-start');
+        $this->appendIf($stretch, 'items-stretch');
+        $this->appendIf($center, 'items-center');
+        $this->appendIf($even, 'justify-evenly');
+        $this->appendIf($between, 'justify-between');
+        $this->appendIf($around, 'justify-around');
+        $this->appendIf($grow, 'flex-grow');
+        $this->appendIf($shrink, 'flex-shrink');
+    }
+
+    public function appendIf($condition, $class, $default = null)
+    {
+        if ($condition) {
+            $this->classes .= " $class";
+        } elseif ($default) {
+            $this->classes .= " $default";
+        }
     }
 
 
