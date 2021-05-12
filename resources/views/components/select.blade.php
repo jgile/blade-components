c2c0b77b26bf73a33e1036e9cd980615c759a3a0
John Gile <me@jgile.com>
2021-05-12 12:38:20 -0700

    wip

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -19,7 +19,7 @@
     {{ $attributes->merge(['class' => 'select']) }}
 >
     @if($placeholder !== null)
-        <option value="" selected>{{ $placeholder }}</option>
+        <option value="" disabled selected>{{ $placeholder }}</option>
     @endif
     @if($allow_empty)
         <option value="" selected></option>
