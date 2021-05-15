e5da2c04689aec25947678a5c869013e627134ac
John Gile <me@jgile.com>
2021-05-15 13:47:47 -0700

    wip

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,22 +1,14 @@
 <div dusk="alert" {{ $attributes }}>
-    <div class="flex">
-        <div>
-            <span class="fa-stack mt-1" style="font-size: .5em">
-                <i class="{{ $attributes->variant('icon-bg') }} fa-stack-2x"></i>
-                <i class="{{ $attributes->variant('icon') }} fa-stack-1x"></i>
-            </span>
-        </div>
-        <div class="flex items-center">
-            @if($title)
-                <h3 class="ml-3 text-sm font-medium">
-                    {{ $title }}
-                </h3>
-            @endif
-            @isset($slot)
-                <div>
-                    <div class="text-sm ml-3">{{ $slot }}</div>
-                </div>
-            @endisset
-        </div>
+    <div class="block fa-stack mt-1" style="font-size: .5em">
+        <i class="{{ $attributes->variant('icon-bg') }} fa-stack-2x"></i>
+        <i class="{{ $attributes->variant('icon') }} fa-stack-1x"></i>
+    </div>
+    <div class="flex-grow">
+        @if($title)
+            <h3 class="ml-3 text-sm font-bold">
+                {{ $title }}
+            </h3>
+        @endif
+        {{ $slot ?? '' }}
     </div>
 </div>
